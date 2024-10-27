9750803d0873f96fac06db739d8cc3188ed2a401
Sebastian Jimenez <sjimnezc@cesde.net>
2024-10-26 19:17:30 -0500

    Sea crea el diseño de formulario de consumo y la caputa de datos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 import { login } from "../controllers/controllerLogin.js";
+import { captureEnergyConsumption } from "../controllers/controllerHome.js";
 
 let btnLogin = document.getElementById('btnLogin');
 if (btnLogin) {
     btnLogin.addEventListener('click', login)
+}
+let btnConsumption = document.getElementById('btnCalculate');
+if (btnConsumption) {
+    btnConsumption.addEventListener('click', captureEnergyConsumption)
 }
